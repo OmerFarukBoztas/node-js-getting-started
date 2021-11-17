24046b23d879554abc94519e3ae4d4c0b9576cff
Omer Faruk BOZTAS <omer@bitality.co>
2021-11-17 19:23:39 +0300

    hello

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
+const sedatpeker =  require("./heroes/sedatpeker.json")
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/sedatpeker', (req,res) => res.send('HELLO WORLD'))
+  .get('/sedatpeker', (req,res) => res.send(sedatpeker))
+
+
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
